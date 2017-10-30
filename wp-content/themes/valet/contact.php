fefa543637a00c8ed961d714155388c813663a69
ferdous-al-quraeshi <ecopavel@gmail.com>
2017-10-30 11:34:45 +0600

    CF7 implemented

--- wp-content/themes/valet/contact.php
+++ wp-content/themes/valet/contact.php
@@ -1,20 +1,4 @@
 <?php
-<<<<<<< HEAD
-
-/*
-Template Name: Contact Us
- */
-
-  get_header();
-
-  get_template_part('slider');
-
-  get_template_part('template-parts/contact-form');
-
-  get_footer();
-
-?>
-=======
 /*
   Template Name: Contact Us
 */
@@ -28,4 +12,3 @@ Template Name: Contact Us
   </section>
 
 <?php get_footer();?>
->>>>>>> 14f3a3c2c39fb1be7cf5ca369cf56587a6151d0d
