452635ab6f5692b87bfe67c04c7ea85ff800cc05
ferdous-al-quraeshi <ecopavel@gmail.com>
2017-10-31 18:36:58 +0600

    styled Contact Us page contents

--- wp-content/themes/valet/contact.php
+++ wp-content/themes/valet/contact.php
@@ -6,9 +6,22 @@
   get_template_part('template-parts/hero-section/hero');?>
 
   <section id="fh5co-main" class="contact-us">
-    <?php
-      the_post();
-      the_content();?>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-8">
+          <?php
+            the_post();
+            the_content();?>
+        </div>
+        <div class="col-md-4">
+          <h3>Need Help?</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non, harum autem quaerat vitae cupiditate, aspernatur est fugit, commodi optio itaque voluptatum! Beatae quae delectus deserunt est ab in sequi blanditiis!</p>
+          <p>
+            <a href="#">info@freehtml5.co</a>
+          </p>
+        </div>
+      </div>
+    </div>
   </section>
 
 <?php get_footer();?>
